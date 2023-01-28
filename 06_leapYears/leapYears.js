d5f4a0eeb300515dec3ac7d980066a48c5171fc5
Scott Dark <47471022+ScottDark@users.noreply.github.com>
2023-01-28 16:17:50 -0500

    Test 4 passed

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -2,7 +2,10 @@
  * Args: Number year */
 const leapYears = function (year) {
   let isLeapYear = Boolean;
-  if (year % 4 === 0 || year % 400 === 0) {
+  if (
+    (year % 4 === 0 && year % 100 !== 0) ||
+    (year % 400 === 0 && year % 100 !== 0)
+  ) {
     return (isLeapYear = true);
   } else {
     return (isLeapYear = false);
