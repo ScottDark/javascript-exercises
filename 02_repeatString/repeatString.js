98b70f3ea78fcac4c111277191683845c650130d
Scott Dark <47471022+ScottDark@users.noreply.github.com>
2023-01-28 11:08:24 -0500

    feat: test 1, 2 passed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,10 +1,11 @@
-const repeatString = function () {
-  // First test repeat hey string three times.
-  let repeatHeyString = "";
-  for (let i = 0; i < 3; i++) {
-    repeatHeyString += "hey";
+// Practice notes: It works by dropping in test info into repeatHeyString and repeatAmount.
+// So the string "hey" is being dropped in and the amount of times it is repeated.
+const repeatString = function (repeatHeyString, repeatAmount) {
+  let concatStringResult = "";
+  for (let i = 0; i < repeatAmount; i++) {
+    concatStringResult += repeatHeyString;
   }
-  return repeatHeyString;
+  return concatStringResult;
 };
 
 // Do not edit below this line
