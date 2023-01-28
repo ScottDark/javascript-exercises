11d42e4498da16d808c7c30c41b6b7b565d83b58
Scott Dark <47471022+ScottDark@users.noreply.github.com>
2023-01-28 12:07:00 -0500

    Test 1 passed, reversed input string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+/* Returns reversed string
+ * normalString - string to be reversed */
+const reverseString = function (normalString) {
+  let reversedStringResult = "";
+  reversedStringResult = normalString.split("").reverse().join("");
+  return reversedStringResult;
 };
 
 // Do not edit below this line
