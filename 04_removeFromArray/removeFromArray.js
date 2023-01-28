c2504f43de6debe201448db18390bbb55ff8e173
Scott Dark <47471022+ScottDark@users.noreply.github.com>
2023-01-28 12:54:41 -0500

    Test 1 complete

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+/* Returns array with removed index
+ * Accepts Array & index to be removed */
+const removeFromArray = function (getArray, indexRemoved) {
+  const index = getArray.indexOf(indexRemoved);
+  if (index > -1) {
+    getArray.splice(index, 1);
+  }
 
+  return getArray;
 };
 
 // Do not edit below this line
