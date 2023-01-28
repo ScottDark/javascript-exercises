21301a211317a1bd4a153e3cae4ecc8431d2f8fc
Scott Dark <47471022+ScottDark@users.noreply.github.com>
2023-01-28 10:31:38 -0500

    feat: first test passed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function () {
+  // First test repeat hey string three times.
+  let repeatHeyString = "";
+  for (let i = 0; i < 3; i++) {
+    repeatHeyString += "hey";
+  }
+  return repeatHeyString;
 };
 
 // Do not edit below this line
