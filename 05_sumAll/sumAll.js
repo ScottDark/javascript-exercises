010688042029e366399e15a7fddced16b3c63289
Scott Dark <47471022+ScottDark@users.noreply.github.com>
2023-01-28 15:10:27 -0500

    Test 1 passed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+/* Return: Sum of every number between arguments
+ * Args: First integer, second integer */
 
+const sumAll = function (getFirstNumber, getSecondNumber) {
+  let sumResult;
+  let max;
+  let min;
+
+  if (getFirstNumber > getSecondNumber) {
+    max = getFirstNumber;
+    min = getSecondNumber;
+  } else {
+    max = getSecondNumber;
+    min = getFirstNumber;
+  }
+
+  sumResult = ((max - min + 1) * (min + max)) / 2;
+  return sumResult;
 };
 
 // Do not edit below this line
