582a3dce4b36b89af8f32ebda697f58db65585cc
Scott Dark <47471022+ScottDark@users.noreply.github.com>
2023-02-02 06:12:36 -0500

    palindromes test 1 pass

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
+const palindromes = function (palindromes) {
+  let splitString = palindromes.split("");
+  let reverseArray = splitString.reverse();
+  let joinArray = reverseArray.join("");
 
+  if (palindromes === joinArray) {
+    return true;
+  } else return false;
 };
 
 // Do not edit below this line
