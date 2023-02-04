11e5a813b48b23f13e80f1fb7ec7aea26149ccb4
Scott Dark <47471022+ScottDark@users.noreply.github.com>
2023-02-04 18:21:26 -0500

    Test 1 passed for find the oldest using reduce()

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  return people.reduce((oldestPerson, currentPerson) => {
+    oldestPersonAge = oldestPerson.yearOfDeath - oldestPerson.yearOfBirth;
+    currentPersonAge = currentPerson.yearOfDeath - currentPerson.yearOfBirth;
 
+    if (oldestPersonAge > currentPersonAge) {
+      return oldestPerson;
+    } else {
+      return currentPerson;
+    }
+  });
 };
 
 // Do not edit below this line
