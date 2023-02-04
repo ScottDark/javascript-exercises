b475027d84f2bfa43af03bc7e464384dbcd02dc6
Scott Dark <47471022+ScottDark@users.noreply.github.com>
2023-02-04 14:37:26 -0500

    All fibonacci tests passed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -3,14 +3,19 @@ const palindromes = function (palindromes) {
   //   let reverseArray = splitString.reverse();
   //   let joinArray = reverseArray.join("");
 
-  let x = palindromes;
+  // let x = palindromes;
 
-  x.replace(/[a-z]+/gi, function (x) {
-    return x.split("").reverse().join("");
+  // x.replace(/[a-z]+/gi, function (x) {
+  //   return x.split("").reverse().join("");
+  // });
+
+  return palindromes.replace(/[a-z]+/gi, function (w) {
+    return w.split("").reverse().join("");
   });
-  if (palindromes === x) {
-    return true;
-  } else return false;
+
+  // if (palindromes === x) {
+  //   return true;
+  // } else return false;
 };
 
 // Do not edit below this line
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function (number) {
+  let x = 1;
+  let y = 1;
+  let z;
+  let i = 0;
 
+  if (number < 0) {
+    return "OOPS";
+  } else {
+    for (i = 2; i < number; i++) {
+      z = x + y;
+      x = y;
+      y = z;
+    }
+    return y;
+  }
 };
 
 // Do not edit below this line
